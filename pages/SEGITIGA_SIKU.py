e6ad1057742b7de422e3f0a00318eb7d59d76dd1
Dr4Ca <55535386+Dr4Ca@users.noreply.github.com>
2022-10-11 19:50:00 +0700

    Update SEGITIGA_SIKU.py

--- pages/SEGITIGA_SIKU.py
+++ pages/SEGITIGA_SIKU.py
@@ -7,7 +7,7 @@ b = st.number_input("Masukan Panjang sisi 2")
 c = st.number_input("Masukan Panjang sisi 3")
 k = a + b + c
 st.subheader(k)
-st.header("LUAS")
+st.header("===LUAS===")
 alas = st.number_input("Masukan panjang Alas")
 tinggi = st.number_input("Masukan Tinggi")
 l = alas * tinggi * 1 / 2
