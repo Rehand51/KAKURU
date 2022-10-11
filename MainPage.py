98578b269f1a8528ca434240e388dcac2db88832
Dr4Ca <55535386+Dr4Ca@users.noreply.github.com>
2022-10-11 19:42:52 +0700

    Create MainPage.py

---
+++ MainPage.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="カクル/KAKURU", page_icon=":tada:", layout="wide")
+
+st.sidebar.success("👆SILAHKAN PILIH👆")
+
+st.title("KALKULATOR RUMUS (**カクル/KAKURU**)")
+st.markdown("""
+    Orang yang terlibat :
+    - Reza Setiawan sebagai penyusun
+    - Hersan Ramadhan sebagai penyusun
+    - Azzam Putra Raihan sebagai Programmer
+    ### Apa Itu KAKURU?
+    **KAKURU** adalah sebuah mesin kalkulator yang berkerja layaknya kalkulator pada umumnya, hanya saja dirancang untuk mempermudah menyelesaikan masalah mengenai rumus matematika.
+    ### Gimana cara kerjanya?
+    Gampang! Cuman pilih lewat sidebar di sini👈 terus tinggal satset satset deh.
+    """)
