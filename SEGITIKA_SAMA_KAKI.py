84eba72787b677acc4b438fa97f87b4cf13e4042
Dr4Ca <55535386+Dr4Ca@users.noreply.github.com>
2022-10-13 05:05:24 +0700

    Fixes and add some stuff
    fix SEGITIGA_SIKU.py
    add alas in SEGITIGA_SIKU

---
+++ SEGITIKA_SAMA_KAKI.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("SEGITIGA SAMA KAKI")
+st.header("===KELILING===")
+s1 = st.number_input("Masukan Panjang Sisi 1:")
+s2 = st.number_input("Masukan Panjang Sisi 2:")
+s3 = st.number_input("Masukan Panjang Sisi 3:")
+k = s1 + s2 + s3
+st.subheader(k)
+
+st.header("===LUAS===")
+a = st.number_input("Masukan Nilai Alas:")
+t = st.number_input("Masukan Nilai Tinggi:")
+l = 1 / 2 * a * t
+st.subheader(l)
