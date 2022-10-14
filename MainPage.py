21bc64263eb357f54e0929473174d0707208e147
Dr4Ca <55535386+Dr4Ca@users.noreply.github.com>
2022-10-14 11:07:11 +0700

    Formsubmit
    Nambahin form submit, sama ngebaruin beberapa hal.

--- MainPage.py
+++ MainPage.py
@@ -15,3 +15,26 @@ st.markdown("""
     ### Gimana cara kerjanya?
     Gampang! Cuman pilih lewat sidebar di sini👈 terus tinggal satset satset deh.
     """)
+
+st.subheader(":mailbox: Gak nemu rumus? Bilang aja!")
+st.write("Website ini masih sepenuhnya dalam pengembangan, jika ada bug atau kesalahan, bisa kontak developer lewat form dibawah👇")
+
+contact_form = """
+<form action="https://formsubmit.co/0e4a07a9d07e5a315c5cb8aaae8d758c" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
