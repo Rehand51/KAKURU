71eca6792af5c48f36f12bd43bf88ed459645606
Dr4Ca <55535386+Dr4Ca@users.noreply.github.com>
2022-10-19 16:49:17 +0700

    Update TRAPESIUM.py

--- pages/TRAPESIUM.py
+++ pages/TRAPESIUM.py
@@ -9,9 +9,3 @@ c = st.number_input("Masukan Nilai Alas C")
 d = st.number_input("Masukan Nilai Alas D")
 k = a + b + c + d
 st.subheader(k)
-st.header("===LUAS===")
-alsa = st.number_input("Masukan Nilai Alas A")
-alsb = st.number_input("Masukan Nilai Alas B")
-t = st.number_input("Masukan Nilai Tinggi")
-l = t * 1 / 2 * alsa + alsb
-st.subheader(l)
