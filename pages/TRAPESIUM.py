23cc4bf7d8a2e81e48a640e637dc046fe366273a
Dr4Ca <55535386+Dr4Ca@users.noreply.github.com>
2022-10-19 16:02:31 +0700

    Create TRAPESIUM.py

---
+++ pages/TRAPESIUM.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("RUMUS TRAPESIUM")
+st.header("===LUAS===")
+alsa = st.number_input("Masukan Nilai Alas A")
+alsb = st.number_input("Masukan Nilai Alas B")
+t = st.number_input("Masukan Nilai Tinggi")
+l1 = alsaa + alsab
+l2 = l1 * t
+l3 = l2 * 1 / 2 
+st.subheader(l3)
