3d8cd6a735b0a44c52bb6cb7b0b5dd41a3d5cc63
Dr4Ca <55535386+Dr4Ca@users.noreply.github.com>
2022-10-19 16:25:14 +0700

    Update TRAPESIUM.py

--- pages/TRAPESIUM.py
+++ pages/TRAPESIUM.py
@@ -1,10 +1,17 @@
 import streamlit as st
 
 st.title("RUMUS TRAPESIUM")
-st.header("===LUAS===")
-alsa = st.number_input("Masukan Nilai Alas A")
-alsb = st.number_input("Masukan Nilai Alas B")
-t = st.number_input("Masukan Nilai Tinggi")
-l = alsa + alsb * t
-l1 = l * 1 / 2
-st.subheader(l1)
+
+st.header("===KELILING===")
+a = st.number_input("Masukan Nilai Alas A")
+b = st.number_input("Masukan Nilai Alas B") 
+c = st.number_input("Masukan Nilai Alas C")
+d = st.number_input("Masukan Nilai Alas D")
+k = a + b + c + d
+st.subheader(k)
+#alsa = st.number_input("Masukan Nilai Alas A")
+#alsb = st.number_input("Masukan Nilai Alas B")
+#t = st.number_input("Masukan Nilai Tinggi")
+#l = alsa + alsb * t
+#l1 = l * 1 / 2 #14^2
+#st.subheader(l1)
