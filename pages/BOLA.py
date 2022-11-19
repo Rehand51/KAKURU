a6ab7b1fe1d20bd2710ca3e3676b4d7a13f62776
Dr4Ca <55535386+Dr4Ca@users.noreply.github.com>
2022-11-19 08:42:31 +0700

    nambahin BOLA

---
+++ pages/BOLA.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.header("KELILING BOLA")
+r = st.number_input("Masukan Panjang Jari - jari")
+k = 2 * 22/7 * r
+kk = 2 * 3.14 * r
+st.write(k, " & ", kk)
+
+st.header("LUAS BOLA")
+l = 4 * 22/7 * r**2
+ll = 4 * 3.14 * r**2
+st.write(l, " & ", ll)
+
+st.write("Kiri menggunakan 22/7, kanan menggunakan 3.14")
