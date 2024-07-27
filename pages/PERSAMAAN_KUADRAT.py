61db6a45e390b6bf020cd764a0397bda95ed350b
Rehand51 <raihanguaming51@gmail.com>
2024-07-27 07:57:11 +0700

    Update PERSAMAAN_KUADRAT.py

--- pages/PERSAMAAN_KUADRAT.py
+++ pages/PERSAMAAN_KUADRAT.py
@@ -2,19 +2,20 @@ import streamlit as st
 
 st.title("PERSAMAAN FUNGSI KUADRAT")
 st.header("DISKRIMINAN")
-st.subheader("Format: (b² -4 a c)")
+
 
 db = st.number_input("Masukan Nilai b", value=0)
 da = st.number_input("Masukan Nilai a", value=0)
 dc = st.number_input("Masukan Nilai c", value=0)
+st.subheader("Format: (b² -4 a c)")
 
 dd = -db**2 - (4 * (da * dc))  # Correction
 st.subheader(dd)
 
 st.header("SUMBU SIMETRI")
-st.subheader("Format: -b/2a")
 xb = st.number_input("Masukan Nilai b:")
 xa = st.number_input("Masukan Nilai a:")
+st.subheader("Format: -b/2a")
 
 try:
     x = -xb / (2 * xa)
@@ -23,10 +24,10 @@ except ZeroDivisionError:
 st.subheader(x)
 
 st.header("NILAI OPTIMUM")
-st.subheader("Format: -D/4a [-(b² - 4ac/4a)]")
 nob = st.number_input("Masukan Nilai b ")
 noa = st.number_input("Masukan Nilai a ")
 noc = st.number_input("Masukan Nilai c ")
+st.subheader("Format: -D/4a [-(b² - 4ac/4a)]")
 
 try:
     nod = (-(nob**2 -4 * noa * noc)) / (4 * noa)
