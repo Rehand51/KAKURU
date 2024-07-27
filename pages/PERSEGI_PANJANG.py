bc89e8c80f688aaa18d75df62447f1b9173bf38b
Rehand51 <raihanguaming51@gmail.com>
2024-07-27 07:59:04 +0700

    Update PERSEGI_PANJANG.py

--- pages/PERSEGI_PANJANG.py
+++ pages/PERSEGI_PANJANG.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.title("PERSEGI PANJANG")
 
-st.header("KELILING")
 p = st.number_input("Masukan Nilai Panjang:")
 l = st.number_input("Masukan Nilai Lebar:")
+st.header("KELILING")
 k = 2 * p + 2 * l
 st.subheader(k)
 st.header("MENCARI PANJANG")
